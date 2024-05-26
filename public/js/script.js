e91125346607e67b07c9e7876c2597af9c794d38
Sebastien MESA <mesasebastien@outlook.fr>
2024-05-26 10:12:45 +0200

    fix du bug de la nav en vue tablette

--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,15 @@ const app = {
     headerNav: document.querySelector('#header-nav'),
     burgerMenu: document.querySelector('#burger-menu'),
     burgerIcon: document.querySelector('#burger-menu-icon'),
+    windowWidth: window.innerWidth,
     lastScroll:0,
 
 
     init() {
+        window.addEventListener('resize', this.getWindowSize.bind(this));
         window.addEventListener('scroll', this.handleNavOnScroll.bind(this));
         this.burgerIcon.addEventListener('click', this.handleBurgerMenu.bind(this));
+
 
     },
 
@@ -15,25 +18,30 @@ const app = {
         let currentScroll = window.scrollY;
         
         if (currentScroll === 0) {
-            this.headerNav.style.height = "7rem";
+            this.headerNav.style.height = "10vh";
             this.headerNav.style.top = "0";
         }
 
-        if (currentScroll > 100) {
-            this.headerNav.style.height = "3.5rem";
-            this.headerNav.style.padding = "0.2rem 0"
-            
-            if (currentScroll < this.lastScroll) {
-                this.headerNav.style.top = 0;
-            } else {
-                this.headerNav.style.top = '-100px';
+        if (this.windowWidth > 992) {
+            if (currentScroll > 100) {
+                this.headerNav.style.height = "3.5rem";
+                
+                if (currentScroll < this.lastScroll) {
+                    this.headerNav.style.top = 0;
+                } else {
+                    this.headerNav.style.top = '-3.5rem';
+                }
+                this.lastScroll = currentScroll;
             }
-            this.lastScroll = currentScroll;
         }
     },
 
     handleBurgerMenu() {
         this.burgerMenu.classList.toggle("burgerOut");
+    },
+
+    getWindowSize() {
+        this.windowWidth = window.innerWidth;
     }
 };
 
